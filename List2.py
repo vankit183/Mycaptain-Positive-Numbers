44fe4879d50bd24c65715c92f31269fba0b4176d
vankit183 <58722883+vankit183@users.noreply.github.com>
2020-09-04 16:07:04 +0530

    Add files via upload

---
+++ List2.py
@@ -0,0 +1,13 @@
+list1 = [12, 14, -95, 3] 
+num = 0
+  
+      
+while(num < len(list1)): 
+      
+     
+    if list1[num] >= 0: 
+        print(list1[num], end = " ") 
+      
+      
+    num += 1
+     
